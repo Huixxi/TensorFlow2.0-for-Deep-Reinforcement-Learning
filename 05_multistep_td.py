4b02a9fb80980e10e470ed20eaaa5bfccf36043b
HU <huxixi.love.story@gmail.com>
2020-01-14 10:34:24 +0800

    Fix a bug, thanks @yoshina621

--- 05_multistep_td.py
+++ 05_multistep_td.py
@@ -172,7 +172,7 @@ class MSTDAgent:  # Multi-Step TD Learning Based on Dueling Double DQN with Prop
             self.n_step_buffer.append(temp_transition)
             if len(self.n_step_buffer) == self.n_step:  # fill the n-step buffer for the first translation
                 # add a multi step transition
-                reward, next_state, done = self.get_n_step_info(self.n_step_buffer, self.gamma)
+                reward, next_obs, done = self.get_n_step_info(self.n_step_buffer, self.gamma)
                 obs, action = self.n_step_buffer[0][:2]
 
             if t == 1:
